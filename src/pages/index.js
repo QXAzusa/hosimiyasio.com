832620218d2b0842db7f56223d7f7923c53779ca
QXAzusa <69675439+QXAzusa@users.noreply.github.com>
2023-02-28 10:23:52 +0800

    Change Project-Name

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,82 @@
-import React from 'react';
-import Layout from '@theme/Layout';
-import Head from '@docusaurus/Head';
-import Translate, {translate} from '@docusaurus/Translate';
-export default function Hello() {
+import React from 'react'
+import clsx from 'clsx'
+import Layout from '@theme/Layout'
+import Link from '@docusaurus/Link'
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import styles from './index.module.css'
+import HomepageFeatures from '../components/HomepageFeatures'
+import Head from '@docusaurus/Head'
+import HeroImg from '../../static/img/Hero.png'
+
+const svgList = [
+  {
+    title: 'bilibili',
+    Svg: require('../../static/img/bilibili.svg').default,
+    link: 'https://space.bilibili.com/148833361',
+  },
+  {
+    title: 'github',
+    Svg: require('../../static/img/github.svg').default,
+    link: 'https://github.com/QXAzusa',
+  },
+]
+const Svg = ({ Svg, color, title, link }) => {
   return (
-    <Layout title="Please Wait" description="Building Page">
-    <head><meta name="robots" content="nOiNdeX, NoFolLoW" /></head>
-    <main class="container margin-vert--xl">
-        <div class="row">
-            <div class="col col--6 col--offset-3">
-                <h1 class="hero__title"><Translate>under-development</Translate></h1>
-            </div>
+    <a href={link} target='_blank'>
+      <Svg className={styles.svg} style={{ fill: color }} />
+    </a>
+  )
+}
+
+
+
+function MyHero() {
+  return (
+    <div className={styles.myHeroContainer}>
+      <div className={styles.leftContainer}>
+        <h1 className={styles.leftContainer_h1}>
+          Always <br /> For Happiness.
+        </h1>
+        <p className={styles.leftContainer_p}>
+          一个热爱探索新事物的家伙 💨
+          <br />
+          在这里记录生活与知识，希望对你也有帮助。
+        </p>
+        <div className={styles.buttonContainer}>
+          {/* <button className={styles.button}>
+            <a className={styles.hero_a} href='/'>
+              Click
+            </a>
+          </button>
+          <span className={styles.buttonLeftText}>
+            Get Started. <br /> 开启学习之旅.
+          </span> */}
+          <div className={styles.svgContainer}>
+            {svgList.map((item, index) => {
+              return <Svg {...item} key={item.title} />
+            })}
+          </div>
         </div>
-    </main>
+      </div>
+      <div className={styles.rightContainer}>
+        <img src='//static.qxazusa.xyz/docusaurus/image/Hero.png' alt='HeroImg' />
+      </div>
+    </div>
+  )
+}
+export default function Home() {
+  const { siteConfig } = useDocusaurusContext()
+  return (
+    <Layout
+      // title={`${siteConfig.title}`}
+      //title='Home'
+      //description='Wiki知识库/vscode/javascript/软件/工具'
+      >
+      {/* <HomepageHeader /> */}
+      <main>
+        <MyHero />
+        {/* <HomepageFeatures /> */}
+      </main>
     </Layout>
-  );
+  )
 }
