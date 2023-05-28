20e7fa185309265d9ce7ec5665a7e8b091565266
浅笑Azusa <69675439+QXAzusa@users.noreply.github.com>
2023-05-28 17:40:12 +0800

    Change favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: '//static.qxazusa.xyz/public/image/favicon.png_favicon',
+  favicon: '//static.qxazusa.xyz/public/image/favicon.png?x-image-process=style/favicon',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
