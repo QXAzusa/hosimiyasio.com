b798ff3d29bbd1bd7100e57c5bee14d2d6222e2e
QXAzusa <15129182-QXAzusa@users.noreply.gitlab.com>
2023-07-15 22:43:35 +0800

    Fix "ReferenceError: none is not defined"

--- static/img/indeximg-change.js
+++ static/img/indeximg-change.js
@@ -3,7 +3,7 @@ var observer = new MutationObserver(function(mutations) {
   mutations.forEach(function(mutation) {
     if (mutation.attributeName === 'data-theme') {
       var imgElement = document.getElementById('indexImg');
-      if (imgElement != none){
+      if (imgElement != null){
         var newValue = htmlElement.getAttribute('data-theme');
         imgElement.src = 'https://static.qxazusa.xyz/sio/image/illustration_hsio_wm.'+ newValue +'.png?x-image-process=style/webp';}
         console.info('Switch To ' + newValue)
