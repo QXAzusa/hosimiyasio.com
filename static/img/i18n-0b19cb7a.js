2fd2775824ab54d133c53d1a4043bb7b72128400
QXAzusa <15129182-QXAzusa@users.noreply.gitlab.com>
2023-07-16 16:46:20 +0800

    Make sure debugger can run

--- static/img/i18n-0b19cb7a.js
+++ static/img/i18n-0b19cb7a.js
@@ -1,30 +1,5 @@
 var script = document.createElement('script');
 script.innerHTML = `
-document.body.onselectstart = function() {
-    return false
-}
-;
-jQuery("img").on("dragstart", function() {
-    return false
-});
-window.oncontextmenu = function() {
-    return false
-}
-;
-document.onkeydown = function(e) {
-    if (e.ctrlKey && (e.keyCode === 65 || e.keyCode === 67 || e.keyCode === 73 || e.keyCode === 74 || e.keyCode === 80 || e.keyCode === 83 || e.keyCode === 85 || e.keyCode === 86 || e.keyCode === 117)) {
-        return false
-    }
-    ;if (e.keyCode == 123) {
-        document.body.innerHTML = "";
-        document.head.innerHTML = "";
-        debugger;
-    }
-}
-;
-document.addEventListener('dragstart', function(event) {
-    event.preventDefault();
-});
 setInterval(function () {debugger;}, 100);
 `;
 document.head.appendChild(script);
