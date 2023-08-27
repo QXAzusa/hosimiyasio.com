943c3086a3bd68bc25a05f007f779864749852f4
QXAzusa <15129182-QXAzusa@users.noreply.gitlab.com>
2023-08-27 22:53:08 +0800

    Make new-index in DocusaurusTheme

--- src/pages/dev-index.js
+++ src/pages/dev-index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import App from './Home';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import Layout from '@theme/Layout'
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext()
   return (
-<App />
+    <Layout
+    >
+    {/* <HomepageHeader /> */}
+    <main>
+    <App />
+      {/* <HomepageFeatures /> */}
+    </main>
+  </Layout>
+
   )
 }
