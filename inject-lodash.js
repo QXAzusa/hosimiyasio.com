726281422baa4018c6df6e0f101473b36070d30a
QXAzusa <git@qxazusa.xyz>
2025-02-03 11:12:36 +0800

    Print website version in console

--- inject-lodash.js
+++ inject-lodash.js
@@ -1,4 +1,5 @@
 if (typeof window !== 'undefined') {
+    console.log('Website Version: 2025-02-03');
     window.addEventListener('keydown', function (e) {
         if (e.ctrlKey && (e.key === 'a' || e.key === 'c' || e.key === 'i' || e.key === 'j' || e.key === 'p' || e.key === 's' || e.key === 'u' || e.key === 'v' || e.key === 'F6')) {
             e.preventDefault();
